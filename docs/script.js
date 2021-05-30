ef8775b7a525cbf97c768038810c44348c5ac1ed
Hell13Cat <46496367+Hell13Cat@users.noreply.github.com>
2021-05-30 20:04:52 +0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -3,7 +3,6 @@ function adapt() {
         var ytvid = document.querySelector('#ytvid');
         var widthScreen = document.documentElement.clientWidth
         var widthVid = Math.round(widthScreen / 100 * 80) + "px";
-        // 1280 720
         var heightVid = Math.round((widthScreen / 100 * 80) / 1280 * 720) + "px";
         ytvid.width = widthVid;
         ytvid.height = heightVid;
