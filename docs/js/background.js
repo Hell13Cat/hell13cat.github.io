b59fd627c833ed53bc2f372f07fa074cd5df6ac2
Hell13Cat <46496367+Hell13Cat@users.noreply.github.com>
2022-03-27 02:48:21 +0300

    Фон страницы

---
+++ docs/js/background.js
@@ -0,0 +1,10 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function mainbg() {
+    let bg_list = ["bg/1.jpg", "bg/2.jpg", "bg/3.jpg"];
+    num_bg = getRandomInt(0, 2);
+    bg_select = bg_list[num_bg]
+    document.getElementById('page').style.background = '#000000 url('+bg_select+') no-repeat';
+}
