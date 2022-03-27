e01142a18d2c5f2d974d68cfe2adb1897e4498d8
Hell13Cat <46496367+Hell13Cat@users.noreply.github.com>
2022-03-27 16:21:31 +0300

    pre4

--- docs/js/background.js
+++ docs/js/background.js
@@ -3,9 +3,17 @@ function getRandomInt(min, max) {
 }
 
 function mainbg() {
-    let bg_list = ["bg/1.jpg", "bg/2.jpg", "bg/3.jpg"];
-    num_bg = getRandomInt(0, 2);
-    bg_select = bg_list[num_bg]
+    let bg_list_pc = ["bg/1.jpg", "bg/2.jpg", "bg/3.jpg"];
+    let bg_list_m = ["bg/11m.jpg", "bg/12m.jpg", "bg/13m.jpg", "bg/21m.jpg", "bg/31m.jpg", "bg/32m.jpg", "bg/33m.jpg"];
+    const pageWidth = document.documentElement.scrollWidth;
+    const pageHeight = document.documentElement.scrollHeight
+    if (pageWidth > pageHeight) {
+        num_bg = getRandomInt(0, 2);
+        bg_select = bg_list_pc[num_bg]
+    } else {
+        num_bg = getRandomInt(0, 6);
+        bg_select = bg_list_m[num_bg]
+    }
     document.getElementById('page').style.background = '#000000 url('+bg_select+') repeat';
     document.getElementById('page').style.backgroundSize = '120%';
 }
