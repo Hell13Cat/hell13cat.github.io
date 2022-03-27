bf50e32858c221b249ae35358dd691759ff49bc5
Hell13Cat <46496367+Hell13Cat@users.noreply.github.com>
2022-03-27 15:27:49 +0300

    pre3

--- docs/js/background.js
+++ docs/js/background.js
@@ -6,5 +6,6 @@ function mainbg() {
     let bg_list = ["bg/1.jpg", "bg/2.jpg", "bg/3.jpg"];
     num_bg = getRandomInt(0, 2);
     bg_select = bg_list[num_bg]
-    document.getElementById('page').style.background = '#000000 url('+bg_select+') no-repeat';
+    document.getElementById('page').style.background = '#000000 url('+bg_select+') repeat';
+    document.getElementById('page').style.backgroundSize = '120%';
 }
